8af440efe1039a88fe32188147066c1cc7163d8e
Micah <micah.iserman@gmail.com>
2021-10-18 20:51:24 -0400

    changes location selection interface

--- site.R
+++ site.R
@@ -16,37 +16,52 @@ page_navbar(
 
 # use `input_` functions to add input elements that affect outputs
 page_menu(
-  input_buttongroup(
-    "Select which", c("district", "county"),
-    display = c("District", "County"), id = "scale"
+  input_select(
+    "Select a Health District", options = "levels",
+    variable = "ID", dataset = "district", dataview = "primary_view",
+    id = "selected_district", reset_button = TRUE
   ),
   input_select(
-    "Selection",
-    options = "levels", variable = "ID",
-    depends = "scale", id = "scope", reset_button = TRUE
+    "Select a County", options = "levels",
+    variable = "ID", dataset = "county", dataview = "primary_view",
+    id = "selected_county", reset_button = TRUE
   ),
-  input_buttongroup("Select Map Shapes", "dataset", default = "district", id = "shapes"),
   input_checkbox("Select a Region Type", options = c("rural", "mixed", "urban"), id = "region_type"),
-  input_slider("Select a Year", variable = "year", id = "year"),
+  input_slider("Select a Year", variable = "year", id = "selected_year"),
   input_select(
     "Select a Variable",
-    options = "variables", id = "variable",
+    options = "variables", id = "selected_variable",
     default = "health_score", depends = "shapes"
   ),
   position = "top",
-  default_open = TRUE
+  default_open = TRUE,
+  conditions = c("", "selected_district", "", "", "")
 )
+
+## `input_variable` can be used to set up logical controls
+input_variable("shapes", list(
+  "selected_district && !selected_county" = "county",
+  "selected_county" = "tract"
+), "district")
+
+input_variable("region_select", list(
+  "shapes == county" = "selected_county"
+), "selected_district")
+
+input_variable("selected_region", list(
+  "selected_county" = "selected_county"
+), "selected_district")
 
 ## `input_dataview` can collect multiple inputs as filters for a shared data view
 input_dataview(
   dataset = "shapes",
-  ids = "scope",
+  ids = "selected_region",
   features = c(type = "region_type"),
   id = "primary_view"
 )
 
 ## `input_rule` can be used to specify if-then conditions for inputs
-input_rule("scale == 'county' & shapes == 'district'", list(shapes = "county"))
+# input_rule("scale == 'county' & shapes == 'district'", list(shapes = "county"))
 
 # use `page_section` to build the page's layout
 page_section(
@@ -55,22 +70,22 @@ page_section(
     type = "row",
     wraps = "col",
     sizes = c(8, 4),
-
+    
     # use `output_` functions to add data displays
     {
       ## add a map
       shape_names <- c("district", "county", "tract")
       shapes <- structure(paste0(
-        "https://uva-bi-sdad.github.io/community/dist/shapes/VA/",
+        "https://raw.githubusercontent.com/uva-bi-sdad/VDH/main/src/dashboard/app/assets/",
         shape_names,
         ".geojson"
       ), names = shape_names)
       output_map(
         shapes,
-        color = "variable",
-        color_time = "year",
+        color = "selected_variable",
+        color_time = "selected_year",
         dataview = "primary_view",
-        click = "scope",
+        click = "region_select",
         subto = "plot0",
         options = list(
           attributionControl = FALSE,
@@ -81,7 +96,7 @@ page_section(
         tiles = list(url = "https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.png"),
       )
     },
-
+    
     ## add an pane to display information about selected and hovered over data
     output_info("Virginia", "Hover over or select a region for more information.", "primary_view", c("map0", "plot0"))
   ),
@@ -90,7 +105,7 @@ page_section(
     {
       ## make plots with plotly
       library(plotly)
-
+      
       ### make a template plot
       template <- plot_ly(
         hoverinfo = "text",
@@ -114,15 +129,15 @@ page_section(
           displayModeBar = FALSE,
           showTips = FALSE
         )
-
+      
       ### then specify dynamic variables, and use the template plot for configuration
       output_plot(
         x = "year",
-        y = "variable",
-        color = "variable",
-        color_time = "year",
+        y = "selected_variable",
+        color = "selected_variable",
+        color_time = "selected_year",
         dataview = "primary_view",
-        click = "scope",
+        click = "region_select",
         subto = "map0",
         options = plotly_json(template, FALSE, FALSE)
       )
@@ -135,4 +150,4 @@ page_section(
 )
 
 # render the site
-site_build(bundle_data = TRUE)
+site_build("../demo_site", bundle_data = TRUE)

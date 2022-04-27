6d85352c25b7b9cf4962736e164be67083324eb1
Micah <micah.iserman@gmail.com>
2022-04-27 18:08:28 -0400

    updates handler

--- docs/functions/api.js
+++ docs/functions/api.js
@@ -1,6 +1,10 @@
 'use strict'
 const DataHandler = require('../data_handler.min.js'),
-  data = new DataHandler(require('../settings.json'))
+  data = new DataHandler(require('../settings.json'), void 0, {
+    tract: require('../tract.json'),
+    county: require('../county.json'),
+    district: require('../district.json'),
+  })
 module.exports.handler = async function (event) {
   return data.export(event.queryStringParameters)
 }

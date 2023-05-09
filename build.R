52bb3f590e24f3a99cc5fc2879e042418acae2b1
Micah <micah.iserman@gmail.com>
2023-05-09 09:23:13 -0400

    adds autoscroll and export options

--- build.R
+++ build.R
@@ -6,17 +6,16 @@ data_add(
     county = "county.csv.xz",
     tract = "tract.csv.xz"
   ),
-  rep(list(list(
+  list(
     ids = list(
       variable = "ID",
       map = "https://uva-bi-sdad.github.io/community/dist/shapes/VA/entity_info.json"
     ),
     time = "time",
     variables = "measure_info.json"
-  )), 3),
+  ),
   dir = "../community_example/docs/data",
-  clean = TRUE,
-  refresh = TRUE
+  clean = TRUE
 )
 
 site_build(
--- site.R
+++ site.R
@@ -80,7 +80,8 @@ page_navbar(
         note = "Limit the number of plot traces that can be drawn, split between extremes of the variable."
       ),
       '<p class="section-heading">Table Options</p>',
-      input_switch("Autoscroll", default_on = TRUE, id = "settings.table_autoscroll"),
+      input_switch("Auto-Sort", default_on = TRUE, id = "settings.table_autosort"),
+      input_switch("Auto-Scroll", default_on = TRUE, id = "settings.table_autoscroll"),
       input_select(
         "Scroll Behavior", c("instant", "smooth", "auto"), "auto",
         id = "settings.table_scroll_behavior", floating_label = FALSE
@@ -194,8 +195,7 @@ page_section(
   type = "col",
   # use `output_` functions to add state and data displays
   output_text(c(
-    "?{starting_shapes != district}State: Virginia[r selected_county]",
-    "?{starting_shapes == district}State: Virginia[r selected_district]",
+    "State: Virginia[r selected_county,selected_district]",
     "? > Health District: {selected_district}[r selected_county]",
     "? > {selected_county}"
   ), class = "compact"),
@@ -375,16 +375,23 @@ page_section(
       ),
       page_popup(
         "Export",
+        page_section(
+          wraps = "col",
+          input_select("Table Format", c("tall", "mixed", "wide"), "mixed", id = "export_table_format"),
+          input_select("File Format", c("csv", "tsv"), "csv", c("CSV", "TSV"), id = "export_file_format")
+        ),
         input_button(
           "Download", "export", dataview = "primary_view", query = list(
             include = "selected_variable",
-            features = list(geoid = "id", name = "name", region_type = "type")
+            features = list(geoid = "id", name = "name"),
+            table_format = "export_table_format", file_format = "export_file_format"
           ), class = "btn-full"
         ),
         input_button(
           "Copy API link", "copy", dataview = "primary_view", query = list(
             include = "selected_variable", dataset = "shapes",
-            features = list(geoid = "id", name = "name", region_type = "type")
+            features = list(geoid = "id", name = "name"),
+            table_format = "export_table_format", file_format = "export_file_format"
           ), class = "btn-full"
         )
       ),

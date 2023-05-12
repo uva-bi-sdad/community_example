53194e6ad9e48d592accc6d21f605b969aa898bf
Micah <micah.iserman@gmail.com>
2023-05-12 07:48:07 -0400

    adds more background shape controls

--- site.R
+++ site.R
@@ -51,6 +51,7 @@ page_navbar(
       '<p class="section-heading">Map Options</p>',
       input_switch("Show Overlay", default_on = TRUE, id = "settings.map_overlay"),
       input_switch("Show Background Shapes", default_on = TRUE, id = "settings.background_shapes"),
+      input_switch("Background On Top", default_on = TRUE, id = "settings.background_top"),
       input_select(
         "Animations", c("fly", "zoom", "none"), "fly",
         note = "Fly animates the whole move to different regions; Zoom only animates changes in zoom level.",
@@ -59,6 +60,10 @@ page_navbar(
       input_number(
         "Outline Weight", "settings.polygon_outline", default = 1.5, step = .5, floating_label = FALSE,
         note = "Thickness of the outline around region shapes."
+      ),
+      input_number(
+        "Background Outline Weight", "settings.background_polygon_outline", default = 2,
+        step = .5, floating_label = FALSE
       ),
       input_number(
         "Overlay Circle Size", "settings.circle_radius", default = 5, step = 1, floating_label = FALSE,
